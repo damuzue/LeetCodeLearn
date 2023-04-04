78a627249b0042cce64b1333ba53b4daa1e8f2a6
damuzue <57245167+damuzue@users.noreply.github.com>
2023-04-04 14:53:21 +0800

    Update LeetCode.py

--- LeetCode.py
+++ LeetCode.py
@@ -177,3 +177,15 @@ class Solution:
             return digits
 
 
+code69:
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r, ans = 0, x, -1
+        while l <= r:
+            mid = (l + r) // 2
+            if mid * mid <= x:
+                ans = mid
+                l = mid + 1
+            else:
+                r = mid - 1
+        return ans

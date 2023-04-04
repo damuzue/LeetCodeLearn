fecc2d9d0da1d76cdf8c891511f30a651f3cfa17
damuzue <57245167+damuzue@users.noreply.github.com>
2023-04-04 11:06:39 +0800

    Update LeetCode.py

--- LeetCode.py
+++ LeetCode.py
@@ -159,3 +159,21 @@ class Solution:
                 if ans:
                     return ans
         return ans
+    
+    
+code66:    
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] != 9:
+                carry = 0
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+        if carry == 1:
+            digits.insert(0,1)
+            return digits
+
+
